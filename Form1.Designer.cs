82b08e683636a69cd9a9ed6ead76c896eadd59bc
unknown <nadatalaat541@gmail.com>
2023-03-15 22:36:21 +0200

    new

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace connect_4_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(701, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(739, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(760, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player 1 start";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(734, 187);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Undo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(819, 187);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(723, 221);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 111);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(783, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(770, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Timer ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 609);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(1500, 1000);
+            this.MinimumSize = new System.Drawing.Size(900, 648);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using System.Media;
+using System.IO;
+
+
+namespace connect_4_project
+{
+    public partial class Form1 : Form
+    {
+        // Declare the sound player object
+        private SoundPlayer playera = new SoundPlayer();
+        private SoundPlayer playerb = new SoundPlayer();
+        private SoundPlayer playerc = new SoundPlayer();
+       //Booleans for the players 
+        public bool player1;
+        public bool player2;
+        int count = 0;
+        //Color for the color of each game piece
+        Color PlayerColor;
+        //array of integers 
+        int[,] board = new int[6, 7];
+        //Turn
+        int turn;
+        //Locations
+        private int previousLoc;
+        private int previousRow;
+        //for timer
+        int sec = 0;
+        //constructor of the Form
+        public Form1()
+        {
+            InitializeComponent();
+            //Set the sound file path of the audio
+            playera.SoundLocation = Path.GetFullPath ("cukur.wav");
+            // Enable looping
+            playera.PlayLooping();
+           //passing value to the players
+            player1 = true;
+            player2 = false;
+            //passing Color 
+            PlayerColor = Color.Blue;
+        }
+        //Method to handle painting of the form
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            this.panel1.BackColor = Color.SlateGray;
+            Graphics g = e.Graphics;
+            Pen line = new Pen(Color.Black);
+            SolidBrush myBrush = new SolidBrush(Color.White);
+            //draw circles
+            for (int y = 10; y <= 710; y += 100)
+            {
+                for (int x = 10; x <= 610; x += 100)
+                {
+                    g.FillEllipse(myBrush, new Rectangle(x, y, 80, 80));
+                }
+            }
+        }
+        //Method to check if the row is empty
+        private int emptyRow(int col)
+        {
+            for (int i = 5; i >= 0; i--)
+            {
+                if (board[i, col] == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        //Method to handle mouse click in the panel
+        //Method that changes the players turn and game piece color
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            button1.Enabled = true;
+            using (Graphics f = this.panel1.CreateGraphics())
+               
+                if (player1)
+                {
+                    label3.Text = "00:00";
+                    sec = 0;
+                    timer1.Start();
+                    turn = 1;
+                    PlayerColor = Color.Blue;
+                    drawGamePiece(e, f);
+                    player1 = false;
+                    player2 = true;
+                    label2.Text = "player 2";
+                    label2.ForeColor = Color.OrangeRed;
+                }
+                else if (player2)
+                {
+                    label3.Text = "00:00";
+
+                    sec = 0;
+                    timer1.Start();
+                    PlayerColor = Color.Red;
+                    turn = 2;
+                    drawGamePiece(e, f);
+                    player2 = false;
+                    player1 = true;
+                    label2.Text = "player 1";
+                    label2.ForeColor = Color.DarkTurquoise;
+                }
+            if(count==0)
+                reset();
+        }
+        // method to take step back for the last player 
+        public void clearGamePiece(Graphics f) 
+        {
+            SolidBrush myBrush = new SolidBrush(Color.White);
+            f.FillEllipse(myBrush, new Rectangle(previousLoc * 100 + 10, previousRow * 100 + 10, 80, 80));
+
+        }
+        
+        //method to handle drawing the pieces, and checking win
+        public void drawGamePiece(MouseEventArgs e, Graphics f)
+        {
+            int col = e.X / 100;
+            int row = emptyRow(col);
+            this.previousRow = row;// save current row 
+            this.previousLoc = col;// save current col
+            SolidBrush myBrush = new SolidBrush(PlayerColor);
+            int win = -1;
+            for (int i = 0; i < 7; i++)
+            {
+                if (row != -1)
+                {
+                    board[row, col] = turn;
+                    if (turn == 1 || turn == 2)
+                    {
+                        f.FillEllipse(myBrush, new Rectangle(col * 100 + 10, row * 100 + 10, 80, 80));
+                    }
+                    win = winSituation(turn);
+
+                }
+            }
+            draw();
+            if (win != -1)
+            {
+                timer1.Stop();
+                label3.Text = "00:00";
+                //Set the sound file path
+                playerb.SoundLocation = Path.GetFullPath("Tada-sound.wav");
+                // Enable looping
+                playerb.Play();
+
+                MessageBox.Show($"player {win} won");
+                reset();
+            }
+
+
+        }
+        //Method to check if there is a winner
+        private int winSituation(int checkPlayer)
+        {
+            //For loop to check vertical win
+            for (int row = 0; row < board.GetLength(0) - 3; row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    if (checkWin(checkPlayer, board[row, col], board[row + 1, col], board[row + 2, col], board[row + 3, col]))
+                        return checkPlayer;
+                }
+            }
+            //For loop to check horizontal win
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1) - 3; col++)
+                {
+                    if (checkWin(checkPlayer, board[row, col], board[row, col + 1], board[row, col + 2], board[row, col + 3]))
+                        return checkPlayer;
+                }
+            }
+            //For loop to check diagonal going up and right from starting piece
+            for (int row = 0; row < board.GetLength(0) - 3; row++)
+            {
+                for (int col = 0; col < board.GetLength(1) - 3; col++)
+                {
+
+                    if (checkWin(checkPlayer, board[row, col], board[row + 1, col + 1], board[row + 2, col + 2], board[row + 3, col + 3]))
+                        return checkPlayer;
+                }
+            }
+            //For loop to check diagonal win going up and to the left from starting piece
+            for (int row = 0; row < board.GetLength(0) - 3; row++)
+            {
+                for (int col = 3; col < this.board.GetLength(1); col++)
+                {
+
+                    if (checkWin(checkPlayer, board[row, col], board[row + 1, col - 1], board[row + 2, col - 2], board[row + 3, col - 3]))
+                        return checkPlayer;
+                }
+            }
+            return -1;
+        }
+        //method to check Play
+        private bool checkWin(int checkPlayer, params int[] number)
+        {
+            foreach (var num in number)
+            {
+                if (num != checkPlayer)
+                {
+                    return false; ;
+                }
+
+            }
+            return true;
+        }
+        //Method to draw blank game board if no player won
+        private void draw()
+        {
+            int drawcount = 0;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 1|| board[i, j] == 2)
+                    {
+                        count++;
+                        drawcount++;
+                    }
+                }
+            }
+         
+            if (drawcount == 42)
+            {
+                //Set the sound file path
+                playerc.SoundLocation = Path.GetFullPath("negative.wav");
+                // Enable looping
+                playerc.Play();
+                timer1.Stop();
+                label3.Text = "00:00";
+                MessageBox.Show("Draw");
+                reset();
+            }
+        }
+        //Method for reset button, resets game when clicked
+        private void reset()
+        {
+            button1.Enabled= false;
+            count = 0;
+            playerb.Stop();
+            playera.PlayLooping();
+            label2.ForeColor = Color.DarkTurquoise;
+            player1 = true;
+            player2 = false;
+            PlayerColor = Color.Blue;
+            board = new int[6, 7];
+            label2.Text = "Player 1 Start";
+            label3.Text = "00:00";
+            sec = 0;
+            panel1.Invalidate();
+        }
+        //Button to undo the last game when clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            board[previousRow, previousLoc] = 0; //Neither player1 or player2
+            clearGamePiece(this.panel1.CreateGraphics());//draw white circle of previous
+            if (player1)
+            {
+                label3.Text = "00:00";
+                sec = 0;
+                timer1.Start();
+                turn = 1;
+                PlayerColor = Color.Blue;
+                player1 = false;
+                player2 = true;
+                label2.Text = "player 2";
+                label2.ForeColor = Color.OrangeRed;
+            }
+            else if (player2)
+            {
+                label3.Text = "00:00";
+                sec = 0;
+                timer1.Start();
+                PlayerColor = Color.Red;
+                turn = 2;
+                player2 = false;
+                player1 = true;
+                label2.Text = "player 1";
+                label2.ForeColor = Color.DarkTurquoise;
+            }
+            button1.Enabled = false;
+        }
+        //Button to reset game when clicked
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (turn!=0||label2.Text=="Player 1 start")
+            {
+               
+                sec++;
+
+                if (sec >= 10)
+                {
+                    //Set the sound file path
+                    playerb.SoundLocation = Path.GetFullPath("Tada-sound.wav");
+                    // Enable looping
+                    playerb.Play();
+                    label3.Text = "00:00";
+                    timer1.Stop();
+                    MessageBox.Show($"player {turn} won");
+                    reset();
+                }
+                
+                    label3.Text = "00:0" + sec;
+            }
+        }
+    }
+}
